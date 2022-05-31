3be696a39660d00c6bb517fc76f8641cfc1de265
Peter Uittenbroek <peter.uittenbroek@wearespindle.com>
2022-05-31 09:50:19 +0200

    Fix must-use-lowercase-with-hypens-for-path-segements using script. Enable/disable desired rules

---
+++ functions/must-use-lowercase-with-hypens-for-path-segements.js
@@ -0,0 +1,26 @@
+const rePathParam = new RegExp('^(\{.*\})$');
+const reRegularHypenedWord = new RegExp('^([\/a-z][a-z0-9\-\/]*)$');
+
+
+const check = (path) => {
+const parts = path.split('/');
+  parts.forEach((part) => {
+    if (part != '' && !part.match(rePathParam) && !part.match(reRegularHypenedWord)){
+      console.log(part, part.match(rePathParam), part.match(reRegularHypenedWord));
+      throw 'Path must only use hypens and lower case!';
+    }
+  });
+};
+
+
+export default path => {
+  try {
+    check(path);
+  } catch (ex) {
+    return [
+      {
+        message: ex,
+      },
+    ];
+  }
+};
